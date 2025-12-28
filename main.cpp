2520e5d116a0e24c406e13797f79d8e146875488
Julien QUENTIN <jqntn88@gmail.com>
2025-12-28 02:21:19 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,32 @@
+﻿#include <GLFW/glfw3.h>
+
+#include <cstdlib>
+
+int
+main()
+{
+
+  if (glfwInit() == GLFW_FALSE) {
+    return EXIT_FAILURE;
+  }
+
+  GLFWwindow* p_window =
+    glfwCreateWindow(800, 800, (const char*)u8"ゴース", nullptr, nullptr);
+  if (p_window == nullptr) {
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  glfwMakeContextCurrent(p_window);
+  glfwSwapInterval(1);
+
+  while (!glfwWindowShouldClose(p_window)) {
+    glfwSwapBuffers(p_window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(p_window);
+  glfwTerminate();
+
+  return EXIT_SUCCESS;
+}
